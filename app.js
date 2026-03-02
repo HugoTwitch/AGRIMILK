426921522620739ee5b26caac666a774ca2b0ae1
Joseph RIGAL <jorigal56@gmail.com>
2026-03-02 07:20:30 +0100

    rework map offset

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const state = {
   selectedCountry: "",
 };
 
-const MAP_ZOOM = 1;
-const MAP_CENTER_OFFSET = [-100, 480];
+const MAP_ZOOM = 1.4;
+const MAP_CENTER_OFFSET = [-100, 600];
 
 const MONTHS = [
   "January",
